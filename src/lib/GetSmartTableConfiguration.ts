fa24c50949d219be66bf20f1353aba7165f2bb96
Julien Moulin <julien@blueanchor.io>
2020-01-15 15:08:08 +0100

    Remove useless folders

--- src/lib/GetSmartTableConfiguration.ts
+++ src/lib/GetSmartTableConfiguration.ts
@@ -3,8 +3,8 @@ import 'reflect-metadata';
 import { SMART_TABLE_CONFIG_KEY } from './constants';
 
 export function GetSmartTableConfiguration(entity) {
-  const smartTableConfig = {};
   const entityInstance = new entity();
+  const smartTableConfig = {};
   const configs = Reflect.getMetadata(SMART_TABLE_CONFIG_KEY, entityInstance);
 
   Object.keys(configs).forEach(key => {
